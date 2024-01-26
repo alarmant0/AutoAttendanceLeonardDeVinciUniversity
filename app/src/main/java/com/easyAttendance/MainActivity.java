5813a017c9345e97fb37f3c01654554aa0969842
alarmant0 <davidmbp57@gmail.com>
2024-01-26 17:08:23 +0100

    Innitial Commit

--- app/src/main/java/com/easyAttendance/MainActivity.java
+++ app/src/main/java/com/easyAttendance/MainActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.outplayesilvccc.R;
 
 public class MainActivity extends AppCompatActivity {
